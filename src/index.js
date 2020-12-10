dfa71912c55d90dc2dd374b75a18b217faaf3bfa
Elinkers <Elinkers@users.noreply.github.com>
2020-12-10 14:46:00 +0800

    code for use

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Auto from './autopic/autopic';
+
+ReactDOM.render(
+  <Auto />,
+  document.getElementById('root')
+);
